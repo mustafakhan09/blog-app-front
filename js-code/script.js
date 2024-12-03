86049179fc53dbad76d2e6bfc97d63731905ace3
Mustafa Khan <112106566+mustafakhan09@users.noreply.github.com>
2024-12-03 14:13:57 +0500

    Update script.js

--- js-code/script.js
+++ js-code/script.js
@@ -2,10 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const currentUrl = window.location.pathname;
     const navButtons = document.querySelectorAll('.nav-btn');
 
-    // Remove 'active' background color from all buttons
     navButtons.forEach(button => button.classList.remove('bg-purple-400'));
 
-    // Add 'bg-purple-400' to the correct button based on the URL
     if (currentUrl.endsWith('index.html')) {
         document.getElementById('homeBtn').classList.add('bg-purple-400');
     } else if (currentUrl.endsWith('post.html')) {
@@ -24,15 +22,13 @@ document.getElementById('logout').addEventListener('click', function() {
 
 
 
-  // Select the sidebar button and the nav-links container (ul)
+
 const sidebarBtn = document.getElementById('sidebarBtn');
 const navLinksContainer = document.querySelector('.nav-links');
 
-// Toggle visibility of the entire nav-links container on button click
 sidebarBtn.onclick = () => {
     navLinksContainer.classList.toggle('hidden');
-     // Add or remove 'hidden' to control display
-    // navLinksContainer.classList.toggle('h-full'); // Add or remove 'hidden' to control display
+
 
 };
 
